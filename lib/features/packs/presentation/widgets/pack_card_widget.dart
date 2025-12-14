e50b05f19af28ac76fecb8ad618aac6c6fc678e3
xEncerx <melixoff.da@yandex.ru>
2025-12-15 00:25:47 +0300

    refactor: update PackCardWidget to use CustomImage and CustomVideo

--- lib/features/packs/presentation/widgets/pack_card_widget.dart
+++ lib/features/packs/presentation/widgets/pack_card_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:octo_image/octo_image.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:video_player/video_player.dart';
+
+import '../../../../shared/shared.dart';
 
 /// A widget that displays a pack card image.
 class PackCardWidget extends StatefulWidget {
@@ -54,11 +54,9 @@ class PackCardWidget extends StatefulWidget {
 }
 
 class _PackCardWidgetState extends State<PackCardWidget> with SingleTickerProviderStateMixin {
-  VideoPlayerController? _videoController;
   late AnimationController _animationController;
   late Animation<double> _animation;
 
-  bool _hasRenderError = false;
   bool _isHovered = false;
 
   @override
@@ -72,70 +70,12 @@ class _PackCardWidgetState extends State<PackCardWidget> with SingleTickerProvid
       parent: _animationController,
       curve: Curves.easeInOut,
     );
-
-    if (widget._isVideo) {
-      _initializeVideo();
-    }
   }
 
   @override
   void dispose() {
     _animationController.dispose();
-    _videoController?.removeListener(_videoErrorListener);
-    _videoController?.dispose();
     super.dispose();
-  }
-
-  void _initializeVideo() {
-    _videoController = widget._isAsset
-        ? VideoPlayerController.asset(widget.cardPath)
-        : VideoPlayerController.networkUrl(
-            Uri.parse(widget.cardPath),
-          );
-
-    _videoController!
-      ..addListener(_videoErrorListener)
-      ..initialize().then(
-        (_) {
-          if (!mounted) return;
-          setState(() {
-            _hasRenderError = false;
-          });
-          _videoController?.setLooping(true);
-          _videoController?.setVolume(0.0);
-          _videoController?.play();
-        },
-        onError: (error) {
-          if (!mounted) return;
-          setState(() {
-            _hasRenderError = true;
-          });
-        },
-      );
-  }
-
-  void _videoErrorListener() {
-    if (_videoController?.value.hasError ?? false) {
-      if (!mounted || !_hasRenderError) return;
-      setState(() {
-        _hasRenderError = true;
-      });
-    }
-  }
-
-  @override
-  void didUpdateWidget(covariant PackCardWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    final pathChanged = oldWidget.cardPath != widget.cardPath;
-    final sourceChanged = oldWidget._isAsset != widget._isAsset;
-    final typeChanged = oldWidget._isVideo != widget._isVideo;
-
-    if (typeChanged || (widget._isVideo && (pathChanged || sourceChanged))) {
-      _videoController?.dispose();
-      _hasRenderError = false;
-      _initializeVideo();
-    }
   }
 
   void _onHoverEnter() {
@@ -195,36 +135,31 @@ class _PackCardWidgetState extends State<PackCardWidget> with SingleTickerProvid
   }
 
   Widget _buildImage(ThemeData theme) {
-    final ImageProvider image = widget._isAsset
-        ? AssetImage(widget.cardPath)
-        : NetworkImage(
-            widget.cardPath,
-            webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
+    return widget._isAsset
+        ? CustomImage.asset(
+            imagePath: widget.cardPath,
+            placeholder: (_, _) => _buildShimmer(theme),
+            errorWidget: (_, _, _) => PackCardWidget._errorImage,
+          )
+        : CustomImage.network(
+            imagePath: widget.cardPath,
+            placeholder: (_, _) => _buildShimmer(theme),
+            errorWidget: (_, _, _) => PackCardWidget._errorImage,
           );
-
-    return OctoImage(
-      image: image,
-      errorBuilder: (_, _, _) => PackCardWidget._errorImage,
-      placeholderBuilder: (_) => _buildShimmer(theme),
-    );
   }
 
   Widget _buildVideo(ThemeData theme) {
-    if (_hasRenderError) return PackCardWidget._errorImage;
-
-    if (_videoController == null || !_videoController!.value.isInitialized) {
-      return _buildShimmer(theme);
-    }
-    return Stack(
-      children: [
-        VideoPlayer(_videoController!),
-        const Positioned.fill(
-          child: ColoredBox(
-            color: Colors.transparent,
-          ),
-        ),
-      ],
-    );
+    return widget._isAsset
+        ? CustomVideo.asset(
+            videoPath: widget.cardPath,
+            placeholder: _buildShimmer(theme),
+            errorWidget: PackCardWidget._errorImage,
+          )
+        : CustomVideo.network(
+            videoPath: widget.cardPath,
+            placeholder: _buildShimmer(theme),
+            errorWidget: PackCardWidget._errorImage,
+          );
   }
 
   Widget _buildShimmer(ThemeData theme) {

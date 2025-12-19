6af83c41a999a43a4293377f8a9e7b6d6de1ca8f
xEncerx <melixoff.da@yandex.ru>
2025-12-19 21:03:33 +0300

    refactor: minor changes

--- backend/lib/features/packs/data/datasources/datasources.dart
+++ backend/lib/features/packs/data/datasources/datasources.dart
@@ -1,3 +1,1 @@
-export 'pack_local_datasource.dart';
-export 'pack_local_datasource_impl.dart';
 export 'pack_remote_api.dart';
--- backend/lib/features/shared/shared.dart
+++ backend/lib/features/shared/shared.dart
@@ -1,5 +1,7 @@
 export 'api/api.dart';
 export 'cache/cache.dart';
+export 'data/data.dart';
 export 'domain/domain.dart';
+export 'infrastructure/infrastructure.dart';
 export 'middleware/middleware.dart';
 export 'scheduler/scheduler.dart';

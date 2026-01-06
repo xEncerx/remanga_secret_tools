9452934fffeaf7c12a9dedea46e596317a0ae9c1
xEncerx <melixoff.da@yandex.ru>
2026-01-06 22:40:18 +0300

    fix: correct documentation

--- backend/lib/features/shared/data/datasources/file_remote_datasource.dart
+++ backend/lib/features/shared/data/datasources/file_remote_datasource.dart
@@ -3,14 +3,14 @@ import 'package:retrofit/retrofit.dart';
 
 part 'file_remote_datasource.g.dart';
 
-/// Remote API for fetching pack data.
+/// Remote data source for file operations.
 @RestApi()
 // ignore: one_member_abstracts
 abstract class FileRemoteDatasource {
   /// Creates a new instance of [FileRemoteDatasource].
   factory FileRemoteDatasource(Dio dio) = _FileRemoteDatasource;
 
-  /// Downloads a image from the given [path].
+  /// Downloads an image from the given [path].
   ///
   /// Returns the bytes of the image.
   @GET('{path}')
